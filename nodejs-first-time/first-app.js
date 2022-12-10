3cf6d0a13435aa1307d77689514ca436218c321d
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2022-12-10 20:30:33 +0700

    add gitignore

---
+++ nodejs-first-time/first-app.js
@@ -0,0 +1,10 @@
+// ? abc
+// * abc
+// ! abc
+// todo abc
+// // abc
+
+//* import fs from node
+const fs = require("fs");
+//* write a file on machine
+fs.writeFileSync("Hello.txt", "Hello from node.js");
